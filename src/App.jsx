726f9ddcca505ac9cbf4faa711dc2fc4f49748b7
amityohan <amit.yohan@kalvium.community>
2025-01-23 12:59:24 +0530

    finished the assignment

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react';
-
+import  React, { useState } from 'react';
+import Bookcard from './components/bookcard';
 function App() {
   const books = [
     {
@@ -34,7 +34,14 @@ function App() {
     },
   ];
 
-  return <>{/* write code to display all the books using */}</>;
+  return(
+      books.map((book)=>{
+        <Bookcard key={book.id}
+                  image={book.image}
+                  genre={book.genre}
+                  aurthor={book.author}/>
+      })
+  );
 }
 
 export default App;
--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,16 @@
-// write the book component code here
+import React from 'react';
+
+function Bookcard({ image, name, genre, author }) {
+  return (
+    <>
+      <div>
+        <img src={image} />
+        <h1>{name}</h1>
+        <p>{genre}<br />
+        Author: {author}</p>
+      </div>
+    </>
+  );
+}
+
+export default Bookcard;
